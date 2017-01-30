b804fbe826c9581ef2194601943b2929cbcc0582
hitoshi54 <dylanclement7@gmail.com>
2017-01-30 19:38:41 +0100

    Added getHeader and getMethod methods

--- httpMethodsData.php
+++ httpMethodsData.php
@@ -1,4 +1,6 @@
 <?php
+namespace library;
+
 class httpMethodsData {
     private static $post;
     private static $get;
@@ -9,53 +11,89 @@ class httpMethodsData {
 
     private static $init = false;
 
+    private static $method;
+    private static $headers;
+
     public static function init() {
-        $method = strtolower($_SERVER['REQUEST_METHOD']);
-        if($method != 'post' && $method != 'get' && $method != 'put' && $method != 'delete' && $method != 'head' && $method != 'patch')
+        self::$method = strtolower($_SERVER['REQUEST_METHOD']);
+        if(self::$method != 'post' && self::$method != 'get' && self::$method != 'put' && self::$method != 'delete' && self::$method != 'head' && self::$method != 'patch')
             return false;
         $values = json_decode(file_get_contents("php://input"));
         if($values === null) {
-            if($method == 'post')
+            if(self::$method == 'post')
                 $values = (object)$_POST;
-            elseif($method == 'get')
+            elseif(self::$method == 'get')
                 $values = (object)$_GET;
         }
 
         self::$$method = $values;
+
+        /* Get all headers, example :
+            Accept: *
+            Accept-Language: en-us
+            Accept-Encoding: gzip, deflate
+            User-Agent: Mozilla/4.0
+            Host: www.example.com
+            Connection: Keep-Alive
+        */
+        self::$headers = getallheaders();
         self::$init = true;
         return true;
     }
 
+    /* Get a value from header */
+    public static function getHeader($name) {
+        if(!self::$init)
+            self::init();
+        var_dump(self::$headers);
+        if(!array_key_exists($name, self::$headers))
+            return false;
+        return self::$headers[$name];
+    }
+
+    /* Get the requested method */
+    public static function getMethod() {
+        if(!self::$init)
+            self::init();
+        return self::$method;
+    }
+
+    /* Get data sent with post in an object */
     public static function post() {
         if(!self::$init)
             self::init();
         return self::$post;
     }
 
+    /* Get data sent with get in an object */
     public static function get() {
         if(!self::$init)
             self::init();
         return self::$get;
     }
 
+    /* Get data sent with put in an object */
     public static function put() {
         if(!self::$init)
             self::init();
         return self::$put;
     }
 
+    /* Get data sent with delete in an object */
     public static function delete() {
         if(!self::$init)
             self::init();
         return self::$delete;
     }
 
+    /* Get data sent with head in an object */
     public static function head() {
         if(!self::$init)
             self::init();
         return self::$head;
     }
 
+    /* Get data sent with patch in an object */
     public static function patch() {
         if(!self::$init)
             self::init();
